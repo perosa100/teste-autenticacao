0f265556f0d35f3d1b6082ba91c8f3fefca032f8
Patrick Perosa <patrickperosapp@gmail.com>
2021-10-02 22:56:22 -0400

    Initial commit

---
+++ src/database.ts
@@ -0,0 +1,48 @@
+import { RefreshTokensStore, UsersStore } from "./types";
+import { v4 as uuid } from "uuid";
+
+export const users: UsersStore = new Map();
+
+export const tokens: RefreshTokensStore = new Map();
+
+export function seedUserStore() {
+  users.set("pp@pp.com", {
+    password: "123456",
+    permissions: ["users.list", "users.create", "metrics.list"],
+    roles: ["administrator"],
+    name: "Patrick Perosa",
+    avatar: "https://github.com/perosa100.png",
+  });
+
+  users.set("victor@pp.com", {
+    password: "123456",
+    permissions: ["users.list", "metrics.list"],
+    roles: ["editor"],
+    name: "Victor Ribeiro",
+    avatar: "https://github.com/devhribeiro.png",
+  });
+}
+
+export function createRefreshToken(email: string) {
+  const currentUserTokens = tokens.get(email) ?? [];
+  const refreshToken = uuid();
+
+  tokens.set(email, [...currentUserTokens, refreshToken]);
+
+  return refreshToken;
+}
+
+export function checkRefreshTokenIsValid(email: string, refreshToken: string) {
+  const storedRefreshTokens = tokens.get(email) ?? [];
+
+  return storedRefreshTokens.some((token) => token === refreshToken);
+}
+
+export function invalidateRefreshToken(email: string, refreshToken: string) {
+  const storedRefreshTokens = tokens.get(email) ?? [];
+
+  tokens.set(
+    email,
+    storedRefreshTokens.filter((token) => token !== refreshToken)
+  );
+}
